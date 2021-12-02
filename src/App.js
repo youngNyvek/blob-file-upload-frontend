21c0f4a831aa46fd0fee2380a57568e94dee22be
Kevin Faria <c055233@yara.com>
2021-12-02 09:10:39 -0300

    Making a upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const testeRef = useRef();
+
+  const axiosTeste = async () => {
+    const formData = new FormData();
+
+    for (let file of testeRef.current.files) {
+      const splitName = file.name.split(".")[1];
+
+      const fileExtension = splitName[splitName.length - 1];
+
+      const changeName = new File([file], `changedname.${fileExtension}`, {
+        type: file.type
+      });
+
+      formData.append("file", changeName);
+    }
+
+    await axios.post("http://localhost:3807/api/fileupload/upload", formData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="file"
+        multiple
+        ref={testeRef}
+        onChange={() => console.log(Array.from(testeRef.current.files))}
+      />
+
+      <button onClick={axiosTeste}>Anexar arquivos</button>
     </div>
   );
-}
+};
 
 export default App;
